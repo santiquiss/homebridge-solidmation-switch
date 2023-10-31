1c4e359c3963eaae15b7806da68f56e5b4bcda47
Santiago Quiss <santiquiss@gmail.com>
2023-10-31 18:34:02 -0300

    cambios en index

--- index.js
+++ index.js
@@ -1,119 +1,91 @@
-/*
-{
-    "accessories": [
-      {
-        "accessory": "Solidmation-Switch",
-        "name": "Accesory Name",
-        "email": "email@domain.com",
-        "password": "password",
-        "deviceName": "Device name in Solidmation",
-        "homeId": "12345",
-        "deviceId": "12345"
-      }
-    ],
-}
-
-*/
-
-
-var Service, Characteristic;
-var request = require("request");
+const request = require("request");
 const lib = require('./lib/switch');
 
-module.exports = function(homebridge){
+let Service, Characteristic;
+
+module.exports = (homebridge) => {
   Service = homebridge.hap.Service;
   Characteristic = homebridge.hap.Characteristic;
-
+  
   homebridge.registerAccessory("homebridge-solidmation-switch", "Solidmation-Switch", SolidmationSwitch);
 };
 
+class SolidmationSwitch {
+  constructor(log, config) {
+    this.log = log;
+    this.name = config.name;
 
-function SolidmationSwitch(log, config) {
-  this.log = log;
+    this.device = new lib.Device();
+    this.characteristicManufacturer = this.device.getDeviceManufacturer();
+    this.characteristicModel = this.device.getDeviceModel();
+    this.characteristicSerialNumber = this.device.getSerialNumber();
 
-  this.name = config.name;
-  this.log(this.name);
+    this.device.setHomeId(config.homeId);
+    this.device.setDeviceId(config.deviceId);
 
-  this.device = new lib.Device()
-  this.characteristicManufacturer = this.device.getDeviceManufacturer()
-  this.characteristicModel = this.device.getDeviceModel()
-  this.characteristicSerialNumber = this.device.getSerialNumber()
-  this.device.setHomeId(config.homeId)
-  this.device.setDeviceId(config.deviceId)
-  var _this = this
-  this.device.login(config.email, config.password)
-    .then(() => {
-      this.device.getStatus()
-        .then(data => {
-          _this.characteristicManufacturer = _this.device.getDeviceManufacturer()
-          _this.characteristicModel = _this.device.getDeviceModel()
-          _this.characteristicSerialNumber = _this.device.getSerialNumber()
-        })
-        .catch(err => {
+    this.initDevice(config).catch(err => this.log(err));
+  }
 
-        })
-    })
-    .catch((err) => {
-    })
+  async initDevice(config) {
+    try {
+      await this.device.login(config.email, config.password);
+      const status = await this.device.getStatus();
+      this.characteristicManufacturer = this.device.getDeviceManufacturer();
+      this.characteristicModel = this.device.getDeviceModel();
+      this.characteristicSerialNumber = this.device.getSerialNumber();
+    } catch (err) {
+      this.log('Error during device initialization:', err);
+    }
+  }
 
-  
-  
-}
-
-SolidmationSwitch.prototype = {
-
-  //Start
-  identify: function(callback) {
+  identify(callback) {
     this.log("Identify requested!");
+    callback();
+  }
 
-    callback(null);
-  },
-  // Required
-  getEstado: function(callback) {
-    this.log("getStatus ")
-	
-	this.device.getStatus()
-      .then(data => {
-        //console.log(data)
-		callback(null, data.encendido)
-      })
-      .catch(err => {
-        callback(err);
-      })
-	
-  },
-  setEstado: function(value, callback) {
-    this.log("setStatus: "+value);
+  async getEstado(callback) {
+    try {
+      const data = await this.device.getStatus();
+      console.log('Data:', data); // This line is for debugging.
+      if (typeof data.encendido === 'boolean') {
+        callback(null, data.encendido);
+      } else {
+        this.log('Unexpected value for data.encendido:', data.encendido);
+        callback(new Error('Unexpected value received'));
+      }
+    } catch (err) {
+      this.log('Error in getEstado:', err);
+      callback(err);
+    }
+  }  
 
-	this.device.switchSetStatus({ isOn: value })
-    var error = null;
-    callback(error);
-  },
-  getName: function(callback) {
+  async setEstado(value, callback) {
+    try {
+      await this.device.switchSetStatus({ isOn: value });
+      callback(null);
+    } catch (err) {
+      callback(err);
+    }
+  }
+
+  getName(callback) {
     this.log("getName :", this.name);
-    var error = null;
-    callback(error, this.name);
-  },
+    callback(null, this.name);
+  }
 
-  getServices: function() {
-
-    // you can OPTIONALLY create an information service if you wish to override
-    // the default values for things like serial number, model, etc.
-    var informationService = new Service.AccessoryInformation();
-
+  getServices() {
+    const informationService = new Service.AccessoryInformation();
     informationService
       .setCharacteristic(Characteristic.Manufacturer, this.characteristicManufacturer)
       .setCharacteristic(Characteristic.Model, this.characteristicModel)
       .setCharacteristic(Characteristic.SerialNumber, this.characteristicSerialNumber);
 
-    var switchService = new Service.Switch(this.name);
-
-    // Required Characteristics
-	switchService
+    const switchService = new Service.Switch(this.name);
+    switchService
       .getCharacteristic(Characteristic.On)
       .on('get', this.getEstado.bind(this))
       .on('set', this.setEstado.bind(this));
 
     return [informationService, switchService];
   }
-};
+}
